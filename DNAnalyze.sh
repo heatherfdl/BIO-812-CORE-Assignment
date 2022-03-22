a413c89e528611ab4dffbc7fb7ab691d07897828
Heather Lounder <93452618+heatherfdl@users.noreply.github.com>
2022-03-22 14:28:31 -0400

    Update DNAnalyze.sh
    updating to match other files

--- DNAnalyze.sh
+++ DNAnalyze.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 echo Generating DNA sequences ...
 
-for i in $(seq -f %03g 1 5)
+
+#This works and outputs files into BIO 812 CORE Assignment folder
+for i in $(seq -f %03g 1 100)
 do
   filename="DNAsequence$i"
-  python -c "import DNAgenforbash;DNAgenforbash.getsequence('$filename', 1000)"
+  python -c "import DNAgen;DNAgen.getsequence('$filename', 1000)"
 done
 
-echo Contetenating files into single csv with base proportions...
+#Count the number of bases in each file
+echo Counting bases ...
+for name in.?#.seq;
+do
+  filename=$(echo${filename:2:9})
+  python -c "import BASEV2;BASEV2.countbases('$filename')" #this function doesnt seem to work but I cannot figure out why 
+done
 
+
+#Combine files into single csv (if previous funtion worked this should operate normally)
+echo Concatenating files together...
+for countfile in ./*count;
+do
+  cat $countfile >> input.csv
+done
   
-  cat DNAsequence*.seq > input.csv
-
-
- 
