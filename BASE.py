ea8e4442de81cf3aab43851dd8977c091ea38f13
Heather Lounder <93452618+heatherfdl@users.noreply.github.com>
2022-03-22 14:30:35 -0400

    Update BASE.py
    updating

--- BASE.py
+++ BASE.py
@@ -1,36 +1,21 @@
-#part B - BASE.py
-#needs to include
-  #1. custom function with parameter filename which allows user to define
-  #name of file to be processed
-  #2. open the file FileName.seq
-  #3. Count number of each base
-  #4. output to screen 4 numbers corresponding to A, G, C, then T, seperatating
-  #each by ","
-  #5. output file will be FileName.count
-  
-#Open file using function "openseq":
-def openseq ():
-  dnatext=open("FASTAseq.seq","r")
-  print(dnatext.read())
-openseq()
+# I dont belive this function is operating corrently but I cannot figure out why
+# if it were to operate correctly then all other code should be 100% operable
 
-#Count number of each base separated by , in order of A, G ,C, T:
-
-for i in openseq():
-  def number_of_bases():
+def countbases(filename):
+  str = filename
+  numbases = ""
+  for i in filename:
     comma = ","
-    numberofbases=str(string.count('A')) + comma + str(string.count('G')) + comma + str(string.count('C')) + comma + str(string.count('T'))
-  print(numberofbases)
-number_of_bases()
-
-
-#output file as FileName.count
-#define path and FileName
-directory="/Users/heather/Desktop/Masters/BIOL 812/BIO-812-CORE-Assignment"
-filename= input("Enter file name: ")
-filepath = directory + filename + ".count"
-filepath.close()
-
-
+    A = str(filename.count('A'))
+    G = str(filename.count('G'))
+    C = str(filename.count('C'))
+    T = str(filename.count('T'))
+    numbases = c(A + comma + G + comma + C + comma + T)
+    print(numbases)
+  output = "Base_num" + ".count"
+  countfile = open(output, "w")
+  countfile.write(numbases)
+  countfile.close
+  return numbases
 
 
