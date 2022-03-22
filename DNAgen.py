7d7d50d1a97b95b1b5a2e1cb2830e69a2c2e2a6e
Heather Lounder <93452618+heatherfdl@users.noreply.github.com>
2022-03-21 22:20:24 -0400

    Update DNAgen.py

--- DNAgen.py
+++ DNAgen.py
@@ -1,34 +1,17 @@
-#Python - DNAgen.py
-#A: script that will generate FASTA file containing randomly generated sequences. 
-  #Includes:
-    #custom function with parameters for FileName and Nb
-    #text file "FileName.seq" that will store random DNA sequence
-    #generate 1 sequence with Nb base pairs in length (randomly chosen, equal probability)
 import random
-import ryr
 import textwrap
-
-#Define length of sequence
-length=100
-#supply list of nucleotides
-bases=["A","G","C","T"]
-print(bases)
-
-#Generate a sequence
 from random import choice
-  #create empty sequence
-  sequence=""
-  #randomly select base and add to sequence(repeat 100 times for length=100)
-  for i in range(length):
-    base=choice(bases)
-    sequence+=base
-print(sequence)
+import sys
+sys.setrecursionlimit(2000)
 
-#create function to generate sequences
-def getsequences(length):
+def getsequence(filename, length):
+  bases=["A","G","C","T"]
   sequence=""
-  for count in range(length):
+  for i in range(length):
     sequence+=choice(bases)
-  return sequence
-getsequences(length)
-#where "getsequences" is the function name
+  output = filename + ".seq"
+  my_file=open(output, "w")
+  my_file.write(sequence)
+  my_file.close()
+  return sequence 
+
